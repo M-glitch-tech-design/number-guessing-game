a57064367a2283612f5b629f07f7fb2749326eed
M-glitch-tech-design <mmr37752@gmail.com>
2025-06-22 17:05:23 +0300

    Add files via upload

---
+++ number guessing game/number guessing game.js
@@ -0,0 +1,7 @@
+let secretNumber = 68;
+if (secretNumber === 69) {
+    console.log("The secret number is 69!");
+}
+else {
+    console.log("this isnt the secret number.");
+}
---
+++ number guessing game/second way.js
@@ -0,0 +1,7 @@
+let secretNumber = prompt("Enter the secret number:");
+secretNumber = Number(secretNumber);
+if (secretNumber === 69) {
+    console.log("The secret number is 69!");
+} else {
+    console.log("this isn't the secret number.");
+}
